392f2a2c68d8af2269d4e361690acfe4e5de920a
syahputrawork98-sketch <syah.putrawork98@gmail.com>
2026-06-14 05:06:35 +0700

    feat: create contactFallback object for profile information backup

--- client/src/fallback/contactFallback.js
+++ client/src/fallback/contactFallback.js
@@ -1,9 +1,9 @@
 export const contactFallback = {
   email: "syah.putrawork98@gmail.com",
   location: "Cimahi, Jawa Barat, Indonesia",
-  phone: "+62 8...",
-  whatsapp: "+628123456789",
+  phone: "+62 85162654466",
+  whatsapp: "+6285162654466",
   linkedin: "https://www.linkedin.com/in/syah-putra-nugraha-292424131/",
   github: "https://github.com/syahputrawork98-sketch",
-  instagram: "https://instagram.com/syah_putra_n"
+  instagram: "https://instagram.com/hallofsyah"
 };
